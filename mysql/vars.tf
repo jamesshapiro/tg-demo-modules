50101e30ceaf09dfd72909b4dae2a6b58f70001b
James Shapiro <jamesshapiro@gmail.com>
2023-01-31 08:27:05 -0500

    remove master password variable

--- mysql/vars.tf
+++ mysql/vars.tf
@@ -28,7 +28,7 @@ variable "master_username" {
   type        = string
 }
 
-variable "master_password" {
-  description = "The password for the master user of the DB"
-  type        = string
-}
+# variable "master_password" {
+#   description = "The password for the master user of the DB"
+#   type        = string
+# }
